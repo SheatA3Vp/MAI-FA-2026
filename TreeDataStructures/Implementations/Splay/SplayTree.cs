e13aec4553a980adf820dc3401b3c123c6c689d4
SheatA3Vp <nveldevatova@gmail.com>
2026-04-20 16:53:49 +0300

    Splay: Implementation

--- TreeDataStructures/Implementations/Splay/SplayTree.cs
+++ TreeDataStructures/Implementations/Splay/SplayTree.cs
@@ -8,20 +8,74 @@ public class SplayTree<TKey, TValue> : BinarySearchTree<TKey, TValue>
 {
     protected override BstNode<TKey, TValue> CreateNode(TKey key, TValue value)
         => new(key, value);
+
+    protected void Splay(BstNode<TKey, TValue>? node)
+    {
+        if (node == null) return;
+
+        while (node.Parent != null)
+        {
+            var parent = node.Parent;
+            var grandparent = parent.Parent;
+
+            if (grandparent == null)
+            {
+                // zig, деда нет, родитель - корень
+                if (node.IsLeftChild) {
+                    RotateRight(parent);
+                } else {
+                    RotateLeft(parent);
+                }
+            }
+            else if (node.IsLeftChild && parent.IsLeftChild)
+            {
+                // zig-zig (оба левые дети)
+                RotateRight(grandparent);
+                RotateRight(parent);
+            }
+            else if (!node.IsLeftChild && !parent.IsLeftChild)
+            {
+                // zig-zig (оба правые дети)
+                RotateLeft(grandparent);
+                RotateLeft(parent);
+            }
+            else if (node.IsLeftChild && !parent.IsLeftChild)
+            {
+                // zig-zag (узел - левый, родитель - правый)
+                RotateRight(parent);
+                RotateLeft(grandparent);
+            }
+            else
+            {
+                // zig-zag (узел - правый, родитель - левый)
+                RotateLeft(parent);
+                RotateRight(grandparent);
+            }
+        }
+        
+        Root = node;
+    }
     
     protected override void OnNodeAdded(BstNode<TKey, TValue> newNode)
     {
-        throw new NotImplementedException();
+        Splay(newNode);
     }
     
-    protected override void OnNodeRemoved(BstNode<TKey, TValue>? parent, BstNode<TKey, TValue>? child)
+    protected override void OnNodeRemoved(BstNode<TKey, TValue>? parent, BstNode<TKey, TValue>? child) { }
+    
+    protected override BstNode<TKey, TValue>? FindNode(TKey key)
     {
-        throw new NotImplementedException();
+        var current = Root;
+        while (current != null)
+        {
+            int cmp = Comparer.Compare(key, current.Key);
+            if (cmp == 0) {
+                Splay(current);
+                return current; 
+            }
+            current = cmp < 0 ? current.Left : current.Right;
+        }
+        return null;
     }
-    
-    public override bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
-    {
-        throw new NotImplementedException();
-    }
-    
+
 }
